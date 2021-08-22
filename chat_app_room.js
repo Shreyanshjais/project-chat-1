9a8e42f406efee6dab5f9a1f0d5f05977e19c9c0
Shreyanshjais <88274304+Shreyanshjais@users.noreply.github.com>
2021-08-22 10:54:19 +0530

    Add files via upload

---
+++ chat_app_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBqX7nvwBpqQ-td-7GTmN41aL0eNviqbcA",
+    authDomain: "kwitter-50ee2.firebaseapp.com",
+    databaseURL: "https://kwitter-50ee2-default-rtdb.firebaseio.com",
+    projectId: "kwitter-50ee2",
+    storageBucket: "kwitter-50ee2.appspot.com",
+    messagingSenderId: "291541942943",
+    appId: "1:291541942943:web:c65477c4fa6800a35858aa",
+    measurementId: "G-VQGQ8XCE31"
+  };
